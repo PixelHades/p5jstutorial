2cb0d265a51fde6c5f33a448096a1e0bd7b342f0
PixelHades <42793209+PixelHades@users.noreply.github.com>
2018-09-10 12:00:52 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup()
+{
+  //create a drawing service 700px wide, 500px tall
+  createCanvas(700,500);
+  
+}
+
+function draw()
+{
+  fill(255,255,255)
+  ellipse(50,50,80,80)
+  //create a background using RGB values
+  stroke(0,0,0)
+  background(74,250,215)
+
+
+ 
+}
